b96f72d6a8974c3fc77d6130199b34e5fc9516d5
Michael Vikström <a19micvi@student.his.se>
2020-04-09 11:45:47 +0200

    Rättade till lite

--- app/src/main/java/com/example/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
@@ -13,6 +14,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView MyWebView;
+
+    public void onCreate(){
+        MyWebView = findViewById(R.id.webvy);
+    }
 
     public void showExternalWebPage(){
         // TODO: Add your code for showing external web page here
