96236831c0fd7ff167c13f9f2d31811af1b6d8ed
Michael Vikström <a19micvi@student.his.se>
2020-04-27 23:30:32 +0200

    enable javascript

--- app/src/main/java/com/example/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void showInternalWebPage(){
 
-
+        webView.loadUrl("file:///android_asset/about.html");
         // TODO: Add your code for showing internal web page here
     }
 
@@ -36,8 +36,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
-
+        webView=findViewById(R.id.my_webview);
+        webView.getSettings().setJavaScriptEnabled(true);
 
         /*
         * Rename your App. Tip: Values->Strings
